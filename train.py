c73fa9693707a2065e4aa2dd9aa43f7a99dae65e
Risong Na <risongna@Risongs-Air.home>
2017-04-01 02:49:19 -0700

    Added retrain_model.py which can be used as a python module

---
+++ train.py
@@ -0,0 +1,9 @@
+import retrain_model
+
+retrain_model.main(bottleneck_dir='../tf_files/bottlenecks',
+                   how_many_training_steps=1000,
+                   model_dir='../tf_files/inception',
+                   output_graph='../tf_files/retrained_graph.pb',
+                   output_labels='../tf_files/retrained_labels.txt',
+                   image_dir='../training')
+
